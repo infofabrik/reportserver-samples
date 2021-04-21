e358fa0f969fc85b8b3ae2a05d35d64d29504266
FM1982 <mr.ml.fwm@t-online.de>
2021-04-21 12:05:22 +0200

    Initial committ of new and improved deleteWriteProtectedUsers.groovy file for deletion of multiple users.

---
+++ src/net/datenwerke/rs/samples/maintenance/deleteWriteProtectedUsers.groovy
@@ -0,0 +1,27 @@
+import net.datenwerke.security.service.usermanager.UserManagerService
+
+/**
+* deleteWriteProtectedUsers.groovy
+* Type: Normal Script
+* Last tested with: ReportServer 3.4.0-6035
+* Deletes multiple users by their user ids even if the users are write protected.
+*/
+
+
+def userManagerService = GLOBALS.getInstance(UserManagerService.class)
+
+
+//Please put the userIds of the users you want to get deleted into the list
+def userIds = [37410,37411,37412]
+
+userIds.each { userId -> 
+    def user = userManagerService.getNodeById(userId)
+  
+    if(null == user){
+        tout.println "User with Id: $userId can not be found!"
+    } else {
+        user.writeProtection = false
+        userManagerService.remove(user)
+        tout.println "User with Id: $userId has been deleted!"
+    }
+}

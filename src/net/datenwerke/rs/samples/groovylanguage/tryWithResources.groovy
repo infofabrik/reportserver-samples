ea71677a1ae64cbde7b75ba4b601e0b4e999deca
Eduardo <66421684+edu-de@users.noreply.github.com>
2021-04-23 15:06:48 +0200

    add tryWithResources.groovy example

---
+++ src/net/datenwerke/rs/samples/groovylanguage/tryWithResources.groovy
@@ -0,0 +1,28 @@
+package net.datenwerke.rs.samples.groovylanguage
+
+import java.nio.file.Paths
+import java.nio.file.Files
+
+/**
+ * tryWithResources.groovy
+ * Type: Normal Script
+ * Last tested with: ReportServer 3.5.0-6037
+ * Demonstrates the groovy's analogous construct to java's try-with-resources.
+ */
+
+def fileLocation = '/path/to/your/file.txt'
+
+def path = Paths.get(fileLocation)
+def sb = new StringBuffer()
+
+// adding withCloseable closure allows groovy to close the resource (analogous to a java try-with-resources)
+Files.newBufferedReader(path).withCloseable{ BufferedReader res -> 
+   def theChar = res.read()
+   
+   while (-1 != theChar) {
+      sb.append((char)theChar)
+      theChar = res.read()
+   }
+}
+
+tout.println sb.toString()

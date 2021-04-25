714c7787f8d4e440a0b51799904304849fc8a848
Eduardo <66421684+edu-de@users.noreply.github.com>
2021-04-25 15:22:51 +0200

    Update deleteWriteProtectedUsers.groovy

--- src/net/datenwerke/rs/samples/maintenance/deleteWriteProtectedUsers.groovy
+++ src/net/datenwerke/rs/samples/maintenance/deleteWriteProtectedUsers.groovy
@@ -9,12 +9,11 @@ import net.datenwerke.security.service.usermanager.UserManagerService
 * Deletes multiple users by their user ids even if the users are write protected.
 */
 
-
 def userManagerService = GLOBALS.getInstance(UserManagerService.class)
 
-
 //Please put the userIds of the users you want to get deleted into the list
-def userIds = [37410,37411,37412]
+//converting the ids to long (l)
+def userIds = [123l, 456l]
 
 userIds.each { userId -> 
     def user = userManagerService.getNodeById(userId)

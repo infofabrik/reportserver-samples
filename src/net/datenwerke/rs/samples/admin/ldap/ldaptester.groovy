820142ac28b6f14b4838332f0bca583778083e8f
edu-de <66421684+edu-de@users.noreply.github.com>
2021-05-07 11:06:08 +0200

    refactor

--- src/net/datenwerke/rs/samples/admin/ldap/ldaptester.groovy
+++ src/net/datenwerke/rs/samples/admin/ldap/ldaptester.groovy
@@ -1,6 +1,6 @@
 package net.datenwerke.rs.samples.admin.ldap
 
-import javax.naming.Context;
+import javax.naming.Context
 import javax.naming.NamingEnumeration
 import javax.naming.directory.Attributes
 import javax.naming.directory.DirContext
@@ -14,10 +14,10 @@ import java.util.logging.Logger
 
 /**
  * ldaptester.groovy
- * Version: 1.0.0
+ * Version: 1.0.1
  * Type: Normal Script
  * Last tested with: ReportServer 3.6.0-6038
- * Tests LDAP settings and prints results into Tomcat logs.
+ * Allows to safely test LDAP settings printing all results found into Tomcat logs.
  * Useful for setting LDAP configuration before setting ldap.cf attributes.
  * The logs should be opened with a text editor without automatic line breaks.
  */
@@ -32,7 +32,7 @@ printToConsole = false
 url = 'ldap://directory.example.com'
 port = 389
 principal = 'CN=ldaptest,CN=Users,DC=directory,DC=example,DC=com'
-credentials = 'ldaptest';
+credentials = 'ldaptest'
 base = 'OU=EXAMPLE,DC=directory,DC=example,DC=com'
 filter = '(|(objectClass=organizationalUnit)(objectClass=user)(objectClass=group))'
 // externalDir not necessary for this test
@@ -68,7 +68,7 @@ userCounter = 0
 ouCounter = 0
 groupCounter = 0
 
-Hashtable env = new Hashtable();
+Hashtable env = new Hashtable()
 env.put(Context.INITIAL_CONTEXT_FACTORY, 'com.sun.jndi.ldap.LdapCtxFactory')
 env.put(Context.PROVIDER_URL, "$url:$port".toString())
 env.put(Context.SECURITY_PRINCIPAL, principal)
@@ -79,8 +79,8 @@ DirContext ctx = null
 
 try {
    def start = System.nanoTime()
-
-   ctx = new InitialDirContext(env);
+   
+   ctx = new InitialDirContext(env)
 
    def returnAttrs = [
       objectClass,
@@ -120,7 +120,7 @@ try {
 
          def objectClassAttr = sr.attributes[objectClass]
          if (!objectClassAttr)
-            printer "$notFound${createAttributePair('objectClass', objectClass)}"
+            printer "$notFound${createAttributePair('objectClass')}"
          else {
             if(objectClassAttr.contains(organizationalUnit_objectClass))
                printOu sr, parentName
@@ -133,11 +133,8 @@ try {
    }
 
    def end = System.nanoTime()
-
    printer '-'*lineLength
-
    printSummary( end - start)
-
    printer '-'*lineLength
 
    printer "LDAP Test finished"
@@ -174,34 +171,34 @@ void printSummary(def duration) {
 
    printer 'Summary'
    printer (sprintf(summaryFormat, 'Total users', userCounter,
-         "${!userCounter? createSummaryNote('user_objectClass', user_objectClass):''}"))
+         "${!userCounter? createSummaryNote('user_objectClass'):''}"))
    printer (sprintf(summaryFormat, 'Total groups', groupCounter,
-         "${!groupCounter? createSummaryNote('group_objectClass', group_objectClass):''}"))
+         "${!groupCounter? createSummaryNote('group_objectClass'):''}"))
    printer (sprintf(summaryFormat, 'Total OUs', ouCounter,
-         "${!ouCounter? createSummaryNote('organizationalUnit_objectClass', organizationalUnit_objectClass):''}"))
+         "${!ouCounter? createSummaryNote('organizationalUnit_objectClass'):''}"))
    printer (sprintf("%-15s %,d ns %s", 'Duration', duration, ''))
 }
 
-String createSummaryNote(def attributeName, def attributeVal) {
-   "Hint: if this is not correct, check your ${createAttributePair(attributeName, attributeVal)} attribute and your filter."
+String createSummaryNote(def attributeName) {
+   "Hint: if this is not correct, check your ${createAttributePair(attributeName)} attribute and your filter."
 }
 
-String createAttributePair(def attributeName, def attributeVal) {
-   "($attributeName:'$attributeVal')"
+String createAttributePair(def attributeName) {
+   "($attributeName:'${this[attributeName]}')"
 }
 
 void printOu(def sr, def parent) {
    ouCounter++
    def formatted = sprintf (format,
-         'OU', getStringAttribute(sr, 'organizationalUnit_name', organizationalUnit_name), '', '', '', guidToString(sr.attributes[guid]), parent)
+         'OU', getStringAttribute(sr, 'organizationalUnit_name'), '', '', '', guidToString(sr.attributes[guid]), parent)
    printer formatted
 }
 
 void printUser(def sr, def parent) {
    userCounter++
    def formatted = sprintf (format,
-         'User', getStringAttribute(sr, 'user_firstname', user_firstname), getStringAttribute(sr, 'user_lastname', user_lastname),
-         getStringAttribute(sr, 'user_username', user_username), getStringAttribute(sr, 'user_mail', user_mail),
+         'User', getStringAttribute(sr, 'user_firstname'), getStringAttribute(sr, 'user_lastname'),
+         getStringAttribute(sr, 'user_username'), getStringAttribute(sr, 'user_mail'),
          guidToString(sr.attributes[guid]), parent)
    printer formatted
 }
@@ -209,7 +206,7 @@ void printUser(def sr, def parent) {
 void printGroup(def sr, def parent) {
    groupCounter++
    def formatted = sprintf (format,
-         'Group', getStringAttribute(sr, 'group_name', group_name), '', '', '', guidToString(sr.attributes[guid]), parent)
+         'Group', getStringAttribute(sr, 'group_name'), '', '', '', guidToString(sr.attributes[guid]), parent)
    printer formatted
 
    // print group members
@@ -222,14 +219,15 @@ void printGroup(def sr, def parent) {
    }
 }
 
-String getStringAttribute(SearchResult sr, String attributeName, String attributeVal){
+String getStringAttribute(SearchResult sr, String attributeName){
 
-   if (!sr.attributes[attributeVal]) return "[$notFound${createAttributePair(attributeName, attributeVal)})]"
+   if (!sr.attributes[this[attributeName]]) 
+      return "[$notFound${createAttributePair(attributeName)})]"
 
    try{
-      return sr.attributes[attributeVal].get().toString()
+      return sr.attributes[this[attributeName]].get().toString()
    }catch(Exception e){
-      tout.println "Failed to retrieve attribute '$attributeVal' from ${sr.nameInNamespace}", e
+      tout.println "Failed to retrieve attribute '${this[attributeName]}' from ${sr.nameInNamespace}", e
    }
 }
 
@@ -240,7 +238,8 @@ void addByte(StringBuffer sb, int k) {
 }
 
 String guidToString(def guidAttribute) {
-   if (!guidAttribute) return "[$notFound${createAttributePair('guid', guid)}]"
+   if (!guidAttribute) 
+      return "[$notFound${createAttributePair('guid')}]"
 
    def bytes = guidAttribute.get()
    StringBuffer sb = new StringBuffer()

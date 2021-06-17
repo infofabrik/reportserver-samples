30243bc9ffdef164dcfcf27effc9ba58b558b4e5
edu-de <66421684+edu-de@users.noreply.github.com>
2021-06-17 10:33:27 +0200

    vetoLargeReportExecution.groovy

---
+++ src/net/datenwerke/rs/samples/admin/vetoLargeReportExecution.groovy
@@ -0,0 +1,42 @@
+import net.datenwerke.rs.core.service.reportmanager.ReportService
+import net.datenwerke.rs.base.service.reportengines.table.TableReportUtils
+import net.datenwerke.rs.core.service.reportmanager.hooks.ReportExecutionNotificationHook
+import net.datenwerke.rs.core.service.reportmanager.exceptions.ReportExecutorException
+
+/**
+ * vetoLargeReportExecution.groovy
+ * Version: 1.0.0
+ * Type: Normal Script
+ * Last tested with: ReportServer 3.7.0-6044
+ * Allows to veto report execution to different formats if a report has a large number of rows.
+ *
+ * You can put this report in onstartup.d to hook up automatically.
+ */
+
+def HOOK_NAME = 'PROHIBIT_EXECUTION'
+def maxNumberOfRows = 10
+/* available are: RS_TABLE (for dynamic list preview), HTML, PDF, EXCEL, CSV */
+def vetoedFormats = ['RS_TABLE', 'EXCEL']
+def errorMessage = '''Your report is too large to be exported, please add filters or similar to limit its output. 
+Current number of rows:'''
+
+def callback = [
+   notifyOfReportExecution : { report, parameterSet, user, outputFormat, configs ->  },
+   notifyOfReportsSuccessfulExecution : { compiledReport, report, parameterSet, user, outputFormat, configs ->
+   },
+   notifyOfReportsUnsuccessfulExecution : { e, report, parameterSet, user, outputFormat, configs ->
+   },
+   doVetoReportExecution: { report, parameterSet, user, outputFormat, configs ->
+      def tableReportUtils = GLOBALS.getInstance(TableReportUtils.class)
+      def reportService = GLOBALS.getInstance(ReportService.class)
+
+      if (outputFormat in vetoedFormats) {
+         def tableReportInformation = tableReportUtils.getReportInformation(report, null)
+         if (tableReportInformation.dataCount > maxNumberOfRows)
+            throw new ReportExecutorException("$errorMessage $tableReportInformation.dataCount")
+      }
+   }
+] as ReportExecutionNotificationHook
+
+GLOBALS.services.callbackRegistry.attachHook(HOOK_NAME, ReportExecutionNotificationHook.class,
+      callback)

083c8c3b970d15d1e4dd1133608dabc305d97706
edu-de <66421684+edu-de@users.noreply.github.com>
2021-12-08 18:10:02 +0100

    more examples and refactor

--- src/net/datenwerke/rs/samples/groovylanguage/tryWithResources.groovy
+++ src/net/datenwerke/rs/samples/groovylanguage/tryWithResources.groovy
@@ -5,76 +5,71 @@ import java.nio.file.Files
 
 /**
  * tryWithResources.groovy
- * Version: 1.1.0
+ * Version: 1.2.0
  * Type: Normal Script
  * Last tested with: ReportServer 4.0.0
  * Demonstrates the groovy's analogous construct to java's try-with-resources.
  */
 
-def fileLocation = '/path/to/your/file.txt'
+fileLocation = '/path/to/your/file.txt'
 
-def separator = '='
-def separatorLength = 40
-def path = Paths.get(fileLocation)
+/* 1: adding withCloseable closure allows groovy to auto-close the resource 
+ * (analogous to a java try-with-resources).
+ * The object must implement Closeable or AutoCloseable in order for this to work. */
+new File(fileLocation).newReader().withCloseable{ resource ->
+   doSomethingWithResource 1, resource
+}
 
-/* 1 */
-def sb = new StringBuilder()
-tout.println '1: ' + separator*separatorLength
-/* adding withCloseable closure allows groovy to close the resource (analogous to a java try-with-resources) */
-Files.newBufferedReader(path).withCloseable{ res ->
-   def theChar = res.read()
+/* 2: for streams you can use withStream() as well. This auto-flushes and auto-closes the stream */
+new File(fileLocation).newInputStream().withStream{ resource ->
+   doSomethingWithResource 2, resource
+}
 
-   while (-1 != theChar) {
-      sb.append((char)theChar)
-      theChar = res.read()
+/* 3: you can also use this with a constructor */
+new FileReader(fileLocation).withCloseable{ resource ->
+   doSomethingWithResource 3, resource
+}
+
+/* 4: if you use nested streams and your outer stream can throw exceptions,
+ * the inner one is not closed automatically, 
+ * so you can use this robust solution.  */
+new FileReader(fileLocation).withCloseable { fileReader ->
+   new BufferedReader(fileReader).withCloseable{ resource ->
+      doSomethingWithResource 4, resource
    }
 }
-tout.println sb.toString()
 
-/* 2 */
-sb = new StringBuilder()
-tout.println '2: ' + separator*separatorLength
-/* you can also use this with a new instance */
-new BufferedReader(new FileReader(path.toFile())).withCloseable{ res ->
-   def theChar = res.read()
-
-   while (-1 != theChar) {
-      sb.append((char)theChar)
-      theChar = res.read()
-   }
-}
-tout.println sb.toString()
-
-/* 3 */
-sb = new StringBuilder()
-tout.println '3: ' + separator*separatorLength
-/* if your outer stream can throw exceptions the inner one is not closed automatically, 
- * so you can use in this case the following more robust solution */
-new FileReader(path.toFile()).withCloseable { fileReader ->
-   new BufferedReader(fileReader).withCloseable{ res ->
-      def theChar = res.read()
-
-      while (-1 != theChar) {
-         sb.append((char)theChar)
-         theChar = res.read()
-      }
-   }
-}
-tout.println sb.toString()
-
-/* 4 */
-sb = new StringBuilder()
-tout.println '4: ' + separator*separatorLength
-/* you can use standard try-with-resources as well. As in 3, we used the more robust solution */
+/* 5: you can use standard java try-with-resources as well. 
+ * As in 4, we use the robust solution */
 try (
-FileReader fileReader = new FileReader(path.toFile());
-BufferedReader res =  new BufferedReader(fileReader)
+FileReader fileReader = new FileReader(fileLocation);
+BufferedReader resource =  new BufferedReader(fileReader)
 ) {
-   def theChar = res.read()
+   doSomethingWithResource 5, resource
+}
+
+
+
+/* 6: do NOT use the following, as the inner stream is not getting closed
+ * if the outer stream throws exceptions */
+//  try (
+//  BufferedReader resource =  new BufferedReader(new FileReader(fileLocation))
+//  ) {
+//      doSomethingWithResource 6, resource
+//  }
+
+
+
+/* here you can do whatever you need with the given resource. 
+ * In this example we just print its contents into the screen. */
+def doSomethingWithResource(example, resource) {
+   tout.println "Example $example: ${'='*40}"
+   def sb = new StringBuilder()
+   def theChar = resource.read()
 
    while (-1 != theChar) {
       sb.append((char)theChar)
-      theChar = res.read()
+      theChar = resource.read()
    }
+   tout.println sb.toString()
 }
-tout.println sb.toString()

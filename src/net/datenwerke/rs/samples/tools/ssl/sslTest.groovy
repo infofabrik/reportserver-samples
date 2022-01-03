21d41cac121d95939c2a7da8b7e929fcfc056d52
edu-de <66421684+edu-de@users.noreply.github.com>
2022-01-03 16:37:53 +0100

    RS-5381

---
+++ src/net/datenwerke/rs/samples/tools/ssl/sslTest.groovy
@@ -0,0 +1,28 @@
+import javax.net.ssl.SSLSocketFactory
+
+/**
+ * sslTest.groovy
+ * Version: 1.0.0
+ * Type: Normal Script
+ * Last tested with: ReportServer 4.0.0
+ * Tests SSL. Based on SSLPoke.java: 
+ * https://confluence.atlassian.com/download/attachments/117455/SSLPoke.java
+ */
+
+def host = 'your_host_or_ip'
+def port = 10636
+
+def sslsocketfactory = SSLSocketFactory.getDefault()
+def sslsocket = sslsocketfactory.createSocket host, port
+
+def in = sslsocket.inputStream
+def out = sslsocket.outputStream
+
+// Write a test byte to get a reaction :)
+out.write 1
+
+while (in.available() > 0) 
+ tout.print in.read()
+  
+
+tout.println 'Successfully connected'

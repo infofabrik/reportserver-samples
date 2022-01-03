dd4132694076fe10c08ef4491875ea088e3e7d6f
edu-de <66421684+edu-de@users.noreply.github.com>
2022-01-03 17:58:45 +0100

    ldapimport description

--- src/net/datenwerke/rs/samples/admin/ldap/ldapimport.groovy
+++ src/net/datenwerke/rs/samples/admin/ldap/ldapimport.groovy
@@ -10,7 +10,7 @@ import net.datenwerke.rs.ldap.service.ldap.LdapService
  * 
  * As of ReportServer 4.0.0 you can use the "ldapimport" terminal command
  * together with the sso/ldap.cf configuration file in order to manually import LDAP users.
- * For scheduling the script periodically, you can use the current script as shown below and schedule it via "scheduleScript".
+ * For scheduling the functionality periodically, you can use the current script as shown below and schedule it via "scheduleScript".
  * 
  * If for any reason you need to use the legacy script, you can find it here:
  * https://github.com/infofabrik/reportserver-samples/blob/main/src/net/datenwerke/rs/samples/admin/ldap/legacyLdapimport.groovy

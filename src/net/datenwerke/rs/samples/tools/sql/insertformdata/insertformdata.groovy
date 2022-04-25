a23f741edd52f77bbc7121f3cce4855b0ee69e7b
edu-de <66421684+edu-de@users.noreply.github.com>
2022-04-25 16:16:18 +0200

    format

--- src/net/datenwerke/rs/samples/tools/sql/insertformdata/insertformdata.groovy
+++ src/net/datenwerke/rs/samples/tools/sql/insertformdata/insertformdata.groovy
@@ -1,10 +1,9 @@
+import java.nio.charset.StandardCharsets
 import java.util.logging.Level
 import java.util.logging.Logger
+
 import groovy.json.JsonSlurper
-import java.net.URLDecoder
-import java.nio.charset.StandardCharsets
 import groovy.sql.Sql
-import groovy.sql.InParameter
 import net.datenwerke.dbpool.DbPoolService
 import net.datenwerke.rs.terminal.service.terminal.TerminalSession
 
@@ -34,18 +33,18 @@ def m = slurper.parseText(URLDecoder.decode(args[0], StandardCharsets.UTF_8.name
 
 def keys = m.keySet()
 dbPoolService.getConnection(datasource.connectionConfig).get().withCloseable { conn ->
-    def sql = new Sql(conn)
-    def insertStmt = """INSERT INTO $table (name,age,email,gender,country,comments) VALUES (
+   def sql = new Sql(conn)
+   def insertStmt = """INSERT INTO $table (name,age,email,gender,country,comments) VALUES (
     ${formatStringValue('name',m)}, ${m.age?:0}, ${formatStringValue('email',m)}, ${formatStringValue('gender',m)}, 
     ${formatStringValue('country',m)}, ${formatStringValue('comments',m)})
     """
-    logger.log(Level.INFO, "INSERT: ${insertStmt}")
-    sql.execute insertStmt.toString()
+   logger.log(Level.INFO, "INSERT: ${insertStmt}")
+   sql.execute insertStmt.toString()
 }
 
 def formatStringValue(key, m) {
-    def val = m."$key"?:'NULL'
-    if (val != 'NULL')
-        val = "'$val'"
-    val
+   def val = m."$key"?:'NULL'
+   if (val != 'NULL')
+      val = "'$val'"
+   val
 }

c8195042badc426ca3d99d2555a03be57fa8f1c9
edu-de <66421684+edu-de@users.noreply.github.com>
2022-12-13 19:10:05 +0100

    loadClasses

--- src/net/datenwerke/rs/samples/tools/nesting/multipleclass/A.groovy
+++ src/net/datenwerke/rs/samples/tools/nesting/multipleclass/A.groovy
@@ -4,23 +4,25 @@ import net.datenwerke.rs.scripting.service.scripting.scriptservices.GlobalsWrapp
 
 /**
  * A.groovy
- * Version: 1.0.2
+ * Version: 1.0.3
  * Type: Normal Script
  * Last tested with: ReportServer 4.5.0
  * Nested script demonstration for nested classes.
  * You can test the script with "exec A.groovy" and it should print C's output.
  */
 
-// use B
-def bClass = GLOBALS.loadClass('myLibraries.groovy', 'net.datenwerke.rs.samples.tools.nesting.multipleclass.B')
+def loadedClasses = GLOBALS.loadClasses('myLibraries.groovy', 
+                                        ['net.datenwerke.rs.samples.tools.nesting.multipleclass.B',
+                                         'net.datenwerke.rs.samples.tools.nesting.multipleclass.C'])
+
+// use B loaded into loadedClasses[0]
 // do not use clazz.newInstance(): https://stackoverflow.com/questions/195321/why-is-class-newinstance-evil
 // use getDeclaredConstructor() instead:
-def bInstance = bClass.getDeclaredConstructor().newInstance()
+def bInstance = loadedClasses[0].getDeclaredConstructor().newInstance()
 tout.println "Using B: ${bInstance.prepareString()}"
 
-// use C
-def cClass = GLOBALS.loadClass('myLibraries.groovy', 'net.datenwerke.rs.samples.tools.nesting.multipleclass.C')
-def cInstance = cClass.getDeclaredConstructor().newInstance()
+// use C loaded into loadedClasses[1]
+def cInstance = loadedClasses[1].getDeclaredConstructor().newInstance()
 tout.println "Using C: ${cInstance.prepareString()}"
 
 

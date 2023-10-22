8028d6d57f7e55b7acdd3e893fe44314749321c1
adrian1703 <65605180+adrian1703@users.noreply.github.com>
2023-10-23 01:58:48 +0200

    Update MARKDOWN_HTML_renderer.groovy

--- src/net/datenwerke/rs/samples/admin/markdown/scriptreport/MARKDOWN_HTML_renderer.groovy
+++ src/net/datenwerke/rs/samples/admin/markdown/scriptreport/MARKDOWN_HTML_renderer.groovy
@@ -4,15 +4,20 @@ import net.datenwerke.rs.fileserver.service.fileserver.entities.FileServerFile
 import net.datenwerke.rs.fileserver.service.fileserver.FileServerService
 import java.nio.charset.StandardCharsets
 import groovy.xml.*
-
-/*
-* This renderer parses a markdown string and converts it to an HTML report.
-* Most command structures will work although there are some exceptions.
-* The renderer requires the file test.md to be of content type "text/markdown" to work.
-*/
+/**
+ * MARKDOWN_HTML_renderer.groovy
+ * Version: 1.0.0
+ * Type: Script report
+ * Last tested with: ReportServer 4.7.0
+ * Uses the markdown-html renderer to parse and render a markdown file (which can be created dynamically)
+ * to an HTML report. 
+ * The renderer requires the file test.md to be of content type "text/markdown" to work.
+ * Input: 
+ * Output: https://github.com/infofabrik/reportserver-samples/blob/main/src/net/datenwerke/rs/samples/admin/svg/scriptreport/DOT_SVG_renderer.png
+ */
 
 def fsService = GLOBALS.getInstance(FileServerService)
-def path =  "resources/rs/test.md"
+def path =  "path/to/markdown_example.md"
 def node = (FileServerFile) fsService.getNodeByPath(path)
 String mdContent = new String(node.getData(), StandardCharsets.UTF_8);
 
